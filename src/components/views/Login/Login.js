23ea30bb0559eff3843c27dfbc321ebb0ba4e184
Rafal Szymanko <szymanko@gmail.com>
2020-06-28 08:37:22 +0200

    Add materialUI components to Login

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import styles from './Login.module.scss';
+import {Link} from 'react-router-dom';
+import { Input, Button, Grid} from '@material-ui/core';
+
 
 
 const Login = () => (
   <div className={styles.component}>
-    <h2>Login View</h2>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Grid container justify="center" spacing={2}>
+          <Grid item>
+            <Input placeholder="username..."></Input>
+          </Grid>
+          <Grid item>
+            <Input placeholder="password..."></Input>
+          </Grid>
+          <Grid item>
+            <Link to={`${process.env.PUBLIC_URL}/`}>
+              <Button variant="contained" color="primary">Sign in</Button>
+            </Link>          
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   </div>
 );
+
+
 
 export default Login;
--- src/components/views/Tables/TablesCurrentlyBooking/TablesCurrentlyBooking.js
+++ src/components/views/Tables/TablesCurrentlyBooking/TablesCurrentlyBooking.js
@@ -4,7 +4,7 @@ import styles from './TablesCurrentlyBooking.module.scss';
 
 const TablesCurrentlyBooking = () => (
   <div className={styles.component}>
-    <h2>Show cbooking details</h2>
+    <h2>Show booking details</h2>
   </div>
 );
 
